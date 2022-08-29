07bd642bd56545ca2402eacc970f3d3774b7aca5
Brett Terpstra <me@brettterpstra.com>
2022-08-29 14:26:00 -0500

    Adding helper script

---
+++ scripts/fixreadme.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+src = 'src/README.md'
+dest = 'README.md'
+current_ver = '2.0.16'
+readme = IO.read(src).force_encoding('ASCII-8BIT').encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => '?')
+
+content = readme.match(/(?<=\<!--README-->)(.*?)(?=\<!--END README-->)/m)[0]
+
+content = "# Gather CLI\n\n#{content}"
+content.gsub!(/\<!--VER-->(.*?)\<!--END VER-->/, current_ver)
+content.gsub!(/<!--GITHUB-->(.*?)<!--END GITHUB-->/m, '\1')
+content.gsub!(/<!--JEKYLL(.*?)-->/m, '')
+
+File.open(dest, 'w') { |f| f.puts(content) }
