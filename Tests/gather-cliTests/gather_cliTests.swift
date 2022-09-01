3e503c43732021ea66ad1eddccc0903862585da6
Brett Terpstra <me@brettterpstra.com>
2022-09-01 07:42:57 -0500

    write some tests

--- Tests/gather-cliTests/gather_cliTests.swift
+++ Tests/gather-cliTests/gather_cliTests.swift
@@ -1,21 +1,27 @@
 import class Foundation.Bundle
 import XCTest
 
-final class gather_cliTests: XCTestCase {
-    func testMetadata() throws {
-        guard #available(macOS 10.13, *) else {
-            return
-        }
+func gatherWith(args: [String], stdin: String?) -> String? {
+    // Some of the APIs that we use below are available in macOS 10.13 and above.
+    guard #available(macOS 10.13, *) else {
+        return nil
+    }
 
-        // Mac Catalyst won't have `Process`, but it is supported for executables.
-        #if !targetEnvironment(macCatalyst)
-
+    // Mac Catalyst won't have `Process`, but it is supported for executables.
+    #if !targetEnvironment(macCatalyst)
+        do {
             let fooBinary = productsDirectory.appendingPathComponent("gather")
 
             let process = Process()
             process.executableURL = fooBinary
 
-            let args = ["--metadata", "https://github.com/vimtaai/critic-markup?query=value&something=else#nowhere"]
+            if stdin != nil {
+                let inpipe = Pipe()
+                let testString = "<p>Testing gather</p>"
+                inpipe.fileHandleForWriting.write(Data(testString.utf8))
+                inpipe.fileHandleForWriting.closeFile()
+                process.standardInput = inpipe
+            }
 
             let pipe = Pipe()
             process.standardOutput = pipe
@@ -25,58 +31,45 @@ final class gather_cliTests: XCTestCase {
             process.waitUntilExit()
 
             let data = pipe.fileHandleForReading.readDataToEndOfFile()
-            let output = String(data: data, encoding: .utf8)
+            return String(data: data, encoding: .utf8)
+        } catch {
+            fatalError("Error running Gather")
+        }
+    #endif
+}
 
-            XCTAssertNotNil(output)
-            XCTAssertNotNil(output!.range(of: "source: https://github.com/vimtaai/critic-markup\n"))
-        #endif
+/// Returns path to the built products directory.
+var productsDirectory: URL {
+    #if os(macOS)
+        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
+            return bundle.bundleURL.deletingLastPathComponent()
+        }
+        fatalError("couldn't find the products directory")
+    #else
+        return Bundle.main.bundleURL
+    #endif
+}
+
+final class gather_cliTests: XCTestCase {
+    func testTitleOnly() throws {
+        let args = ["--title-only", "https://github.com/vimtaai/critic-markup"]
+        let output = gatherWith(args: args, stdin: nil)
+        XCTAssertNotNil(output)
+        XCTAssertEqual(output!, "vimtaai/critic-markup\n")
+    }
+
+    func testMetadata() throws {
+        let args = ["--metadata", "https://github.com/vimtaai/critic-markup?query=value&something=else#nowhere"]
+        let output = gatherWith(args: args, stdin: nil)
+        XCTAssertNotNil(output)
+        XCTAssertNotNil(output!.range(of: "source: https://github.com/vimtaai/critic-markup\n"))
     }
 
     func testStdin() throws {
-        // Some of the APIs that we use below are available in macOS 10.13 and above.
-        guard #available(macOS 10.13, *) else {
-            return
-        }
-
-        // Mac Catalyst won't have `Process`, but it is supported for executables.
-        #if !targetEnvironment(macCatalyst)
-
-            let fooBinary = productsDirectory.appendingPathComponent("gather")
-
-            let process = Process()
-            process.executableURL = fooBinary
-
-            let args = ["--stdin", "--html", "--no-readability"]
-
-            let inpipe = Pipe()
-            let testString = "<p>Testing gather</p>"
-            inpipe.fileHandleForWriting.write(Data(testString.utf8))
-            inpipe.fileHandleForWriting.closeFile()
-            process.standardInput = inpipe
-
-            let pipe = Pipe()
-            process.standardOutput = pipe
-
-            process.arguments = args
-            try process.run()
-            process.waitUntilExit()
-
-            let data = pipe.fileHandleForReading.readDataToEndOfFile()
-            let output = String(data: data, encoding: .utf8)
-
-            XCTAssertEqual(output, "Testing gather\n")
-        #endif
-    }
-
-    /// Returns path to the built products directory.
-    var productsDirectory: URL {
-        #if os(macOS)
-            for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
-                return bundle.bundleURL.deletingLastPathComponent()
-            }
-            fatalError("couldn't find the products directory")
-        #else
-            return Bundle.main.bundleURL
-        #endif
+        let args = ["--stdin", "--html", "--no-readability"]
+        let testString = "<p>Testing gather</p>"
+        let output = gatherWith(args: args, stdin: testString)
+        XCTAssertNotNil(output)
+        XCTAssertEqual(output, "Testing gather\n")
     }
 }
