44b84a0a60942b17e47a98532af23f2519c43789
Brett Terpstra <me@brettterpstra.com>
2022-09-02 11:40:36 -0500

    Preparing for 2.0.38 release

--- Sources/gather/gather.swift
+++ Sources/gather/gather.swift
@@ -4,7 +4,7 @@ import Cocoa
 import Foundation
 import HTML2Text
 import Readability
-var VERSION = "2.0.37"
+var VERSION = "2.0.38"
 
 var acceptedAnswerOnly = false
 var disableReadability = false
